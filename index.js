17675ff4903a76d8da7f20568437e31fe5dfa017
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2025-07-15 11:16:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,36 +17,36 @@ function showLanguage(lang){
   }
 }
 
-// document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded',() => {
 
-//   const languageMap = {
-//     'en': 'en',
-//     'en-US':'en',
-//     'ja': 'jp',
-//     'ja-JP':'jp',
-//     'zh': 'ch_simplify',
-//     'zh-CN': 'ch_simplify',
-//     'zh-TW': 'ch_traditional',
-//     'fr': 'fr',
-//     'fr-FR': 'fr',
-//     'it':'it',
-//     'it-IT': 'it',
-//     'es':'es',
-//     'es-ES':'es',
-//     'es-419': 'es_latino',
-//     'es-MX': 'es_latino',
-//     'es-AR': 'es_latino',
-//     'es-CL': 'es_latino'
-//   };
+  const languageMap = {
+    'en': 'en',
+    'en-US':'en',
+    'ja': 'jp',
+    'ja-JP':'jp',
+    'zh': 'ch_simplify',
+    'zh-CN': 'ch_simplify',
+    'zh-TW': 'ch_traditional',
+    'fr': 'fr',
+    'fr-FR': 'fr',
+    'it':'it',
+    'it-IT': 'it',
+    'es':'es',
+    'es-ES':'es',
+    'es-419': 'es_latino',
+    'es-MX': 'es_latino',
+    'es-AR': 'es_latino',
+    'es-CL': 'es_latino'
+  };
 
-//   const userLang = navigator.language || navigator.userLanguage;
-//   const languageID = languageMap[userLang] || 'en';
+  const userLang = navigator.language || navigator.userLanguage;
+  const languageID = languageMap[userLang] || 'en';
 
-//   showLanguage(languageID);
+  showLanguage(languageID);
 
   
 
-document.addEventListener('DOMContentLoaded',() => {
+// document.addEventListener('DOMContentLoaded',() => {
 
   document.getElementById('en-btn').addEventListener('click', () => showLanguage('en'));
   document.getElementById('jp-btn').addEventListener('click', () => showLanguage('jp'));
